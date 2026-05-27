077ff485ec4e9f55765e967c947bde6d9f30c0da
Hardik Jain <jainhardik651@gmail.com>
2026-05-27 16:09:26 +0530

    Episode 03

---
+++ Episode 03/app.js
@@ -0,0 +1,8 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+const heading = React.createElement("h1", {id: "heading"}, "I'm heading from React");
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+
+root.render(heading);
+//updated
