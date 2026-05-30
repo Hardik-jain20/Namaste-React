168c69710cfe51e246fa45506a59eaabc9bf2333
Hardik Jain <jainhardik651@gmail.com>
2026-05-30 18:14:11 +0530

    Episode 05 - OPtimising code and using useState()

--- Episode 05/components/Body.js
+++ Episode 05/components/Body.js
@@ -1,14 +1,24 @@
 import resList from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
+import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+    // local state variable
+    const [lisOfRestaurants, setListOfRestaurant] = useState(resList);
+    
     return (
         <div className="body">
-            <div className="search">
-                Search
+            <div className="filterButton">
+                <button className="btn" onClick={()=> {
+                    const filteredList = lisOfRestaurants.filter(
+                    (res) => res.info.avgRating > 4.2
+                    );
+                    setListOfRestaurant(filteredList);
+                }}> Top Rated Restaurant</button>
             </div>
             <div className="restaurantContainer">
-                {resList.map((restaurant) => <RestaurantCard Key = {restaurant.info.id} resData = {restaurant}/>)}
+                {lisOfRestaurants.map((restaurant) => <RestaurantCard Key = {restaurant.info.id} resData = {restaurant}/>)}
             </div>
         </div>
     )
