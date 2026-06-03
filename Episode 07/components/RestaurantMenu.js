7ce0df6a5f9b1c83e069b678c8ae014fe2eef6b4
Hardik Jain <jainhardik651@gmail.com>
2026-06-03 18:12:21 +0530

    Fix itemCards error

--- Episode 07/components/RestaurantMenu.js
+++ Episode 07/components/RestaurantMenu.js
@@ -24,8 +24,8 @@ const RestaurantMenu = () => {
 
     if (resInfo === null ) return <Shimmer />;
     const { name, cuisines, costForTwoMessage, areaName } = resInfo?.cards[2]?.card?.card.info;
-    const { itemCards } = resInfo.cards[5].groupedCard.cardGroupMap.REGULAR.cards[3].card.card;
-    console.log(itemCards);
+    const { itemCards } = resInfo.cards[5].groupedCard.cardGroupMap.REGULAR.cards[1].card.card || resInfo.cards[5].groupedCard.cardGroupMap.REGULAR.cards[3].card.card;
+    //console.log(itemCards);
     
     return (
         <div className="menu">
