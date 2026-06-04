3e1cf070e1cbad76079e0939fd98d1bcf34e69fe
Hardik Jain <jainhardik651@gmail.com>
2026-06-04 14:47:12 +0530

    how to define state variable in cbc

--- Episode 08/components/UserClass.js
+++ Episode 08/components/UserClass.js
@@ -3,11 +3,19 @@ import React from "react";
 class UserClass extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            count: 0,
+            count2: 2,
+
+        };
     }
     render() {
         const {name, location, contact} = this.props; 
+        const {count} = this.state;
         return (
             <div className="user">
+                <h2>Count: {count}</h2>
+                <h2>Count2: {this.state.count2}</h2>
                 <h2>Name: {name}</h2>
                 <h3>Location: {location}</h3>
                 <h4>Contact: {contact}</h4>
