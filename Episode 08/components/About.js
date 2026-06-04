8ea22c5d5034a0d41a216a1b84a1b5f779f9aa5a
Hardik Jain <jainhardik651@gmail.com>
2026-06-04 15:28:52 +0530

    Understanded the parent-child life cycle

--- Episode 08/components/About.js
+++ Episode 08/components/About.js
@@ -1,17 +1,29 @@
 import User from "./User";
 import UserClass from "./UserClass";
+import { Component } from "react";
 
-const About = () => {
-    return (
-        <div className="about">
-            <h1>About Us!!!</h1>
-            <h2>Foodie Woodie 😋 is a trusted food delivering platform
-                loved by many hungry faces.
-            </h2>
-            <User name = {"Hardik function"} location = {"Nathdwara"} contact = {"0123456789"}/>
-            <UserClass name = {"Hardik class"} location = {"Nathdwara"} contact = {"0123456789"}/>
-        </div>
-    )
+class About extends Component {
+    constructor(props) {
+        super(props);
+        console.log("Parent constructor.");
+
+    }
+
+    componentDidMount() {
+        console.log("Parent COmponent Mount.");
+    }
+    render() {
+        console.log("Parent render.")
+        return (
+            <div className="about">
+                <h1>About Us!!!</h1>
+                <h2>Foodie Woodie 😋 is a trusted food delivering platform
+                    loved by many hungry faces.
+                </h2>
+                <UserClass name = {"Hardik class"} location = {"Nathdwara"} contact = {"0123456789"}/>
+            </div>
+        )
+    }
 }
 
 export default About;
--- Episode 08/components/UserClass.js
+++ Episode 08/components/UserClass.js
@@ -4,18 +4,28 @@ class UserClass extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            count: 0,
-            count2: 2,
-
+            count: 0
         };
+        
+        console.log("Child Constructor");
     }
+
+    componentDidMount() {
+        console.log("Child Component did mount. ");
+    }
+
     render() {
+        console.log("Child render");
         const {name, location, contact} = this.props; 
         const {count} = this.state;
         return (
             <div className="user">
                 <h2>Count: {count}</h2>
-                <h2>Count2: {this.state.count2}</h2>
+                <button onClick={() => {
+                    this.setState({
+                        count: this.state.count + 1,
+                    });
+                }}>Count ⬆️</button>
                 <h2>Name: {name}</h2>
                 <h3>Location: {location}</h3>
                 <h4>Contact: {contact}</h4>
