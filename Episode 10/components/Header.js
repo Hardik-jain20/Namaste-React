c1a59ade12c5ee9bcd43a71bf621481e30156db0
Hardik Jain <jainhardik651@gmail.com>
2026-06-05 19:36:34 +0530

    Added tailwind to the app

--- Episode 10/components/Header.js
+++ Episode 10/components/Header.js
@@ -11,30 +11,30 @@ const Header = () => {
     const onlineStatus = useOnlineStatus();
 
     return (
-        <div className="header">
-            <div className="logo">
-                <img className="logo-img" src={LOGO_URL} alt="Foodie Woodie Logo" />
+        <div className="header flex justify-between border-2 border-black border-solid">
+            <div className="logo flex items-center ">
+                <img className="logo-img w-32" src={LOGO_URL} alt="Foodie Woodie Logo" />
                 <h2 className="companyName">Foodie Woodie 😋</h2>
                 </div>
             <div className="nav-items">
-                <ul>
+                <ul className="flex m-4 p-4">
                     
-                    <li>
+                    <li className="px-4">
                         <Link to="/grocery">Grocery</Link>
                     </li>
-                    <li>
+                    <li className="px-4">
                         <Link to="/">Home</Link>
                     </li>
-                    <li>
+                    <li className="px-4">
                         <Link to="/about">About Us</Link>
                     </li>
-                    <li>
+                    <li className="px-4">
                         <Link to="/contact">Conatct Us</Link>
                     </li>
-                    <li className="cart">
+                    <li className="cart px-4">
                         <Link to="/Cart">🛒 Cart</Link>    
                     </li>
-                    <button className="login-btn" onClick={() => {
+                    <button className="login-btn px-4" onClick={() => {
                         btnNameReact === "Login"
                         ? setBtnNameReact("Logout")
                         : setBtnNameReact("Login");
