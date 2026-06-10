24461feb92429746a1a9fadca48a40c1b9727cac
Hardik Jain <jainhardik651@gmail.com>
2026-06-10 18:29:01 +0530

    Added the accordian feature to menu list

--- Episode 11/components/RestaurantCategories.js
+++ Episode 11/components/RestaurantCategories.js
@@ -1,19 +1,24 @@
 import ItemList from "./ItemList";
+import { useState } from "react";
 
 const RestaurantCategories = ({ data }) => {
     //console.log(data);
     const {title, itemCards, image} = data;
+    const [showItems, setShowItems] = useState(false);
+
     return(
         <div>
             {/* Accordian Header */}
             <div className="w-6/12 mx-auto my-2 shadow-lg p-4 bg-gray-100 ">
-                <div className="flex justify-between">
+                <div className="flex justify-between cursor-pointer" onClick={() => {
+                    setShowItems(!showItems);
+                }}>
                     <span className="font-bold text-lg">{title} ({itemCards.length})</span>
-                    <span>{"▽"}</span>
+                    <span>{!showItems ? "▽" : "△"}</span>
                 </div>
                 
                 {/* Accordian Body */}
-                <ItemList items = {data.itemCards}/>
+                {showItems && <ItemList items = {data.itemCards}/>}
             </div>
         </div>
     )
